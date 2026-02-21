a0e95b3d4a6dfb2507802e581e7157bc960b9708
chadhas282-afk <chadhas282@gmail.com>
2026-02-21 11:08:24 +0530

    add logic to starting screen

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 const world = document.getElementById("world")
+const scoreEL = document.getElementById("score")
+const finalScoreEl = document.getElementById("final-score");
+const startBtn = document.getElementById("start-btn");
+const restartBtn = document.getElementById("restart-btn");
+const startScreen = document.getElementById("start-screen");
+const gameOverScreen = document.getElementById('game-over-screen');
+
+let score = 0;
+let isGameOver = true;
+let PlayerPos = 1;
+let lanes = [40, 130, 220];
+
+const player = document.getElementById("div")
+player.classList.add("player")
+world.appendChild(player);
+
+function startGame(){
+    score = 0;
+    isGameOver = false;
+    scoreEL.innerText = score;
+    startScreen.classList.add("hidden");
+    gameOverScreen.classList.add("hidden")
+    document.getElementById("score-display").classList.remove("hidden")
+    gameLoop();
+}
+
+function gameLoop(){
+    if (isGameOver) return;
+
+    if(Math.random < 0.02){
+        createObstacle();
+    }
+    score++;
+    scoreEl.innerText = Math.floor(score / 10);
+    requestAnimationFrame(gameLoop);
+}
+
